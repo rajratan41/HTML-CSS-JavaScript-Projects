3363c1b064a67370550323b4978bc96c3f1deeb4
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-05-01 05:27:31 +0000

    created functionality search and filter

--- JavaScript/08-SearchFilter/script.js
+++ JavaScript/08-SearchFilter/script.js
@@ -4,49 +4,49 @@ let products = {
   data: [
     {
       productName: "Regular White T-shirt",
-      category: "Topwear",
+      category: "topwear",
       price: "30",
       image: "./assets/white-tshirt.jpg",
     },
     {
       productName: "Beige Short Skirt",
-      category: "Bottomwear",
+      category: "bottomwear",
       price: "49",
       image: "./assets/short-skirt.jpg",
     },
     {
       productName: "Sporty SmartWatch",
-      category: "Watch",
+      category: "watch",
       price: "99",
       image: "./assets/sporty-smartwatch.jpg",
     },
     {
       productName: "Basic Knitted Top",
-      category: "Topwear",
+      category: "topwear",
       price: "29",
       image: "./assets/knitted-top.jpg",
     },
     {
       productName: "Black Leather Jacket",
-      category: "Jacket",
+      category: "jacket",
       price: "129",
       image: "./assets/black-leather-jacket.jpg",
     },
     {
       productName: "Stylish Pink Trousers",
-      category: "Bottomwear",
+      category: "bottomwear",
       price: "89",
       image: "./assets/pink-trousers.jpg",
     },
     {
       productName: "Brown Men's Jacket",
-      category: "Jacket",
+      category: "jacket",
       price: "189",
       image: "./assets/brown-jacket.jpg",
     },
     {
       productName: "Comfy Gray Pants",
-      category: "Bottomwear",
+      category: "bottomwear",
       price: "49",
       image: "./assets/comfy-gray-pants.jpg",
     },
@@ -57,7 +57,7 @@ for (let i of products.data) {
   // Create Card
   let card = document.createElement("div");
   //  Card should have category and should stay hidden initally
-  card.classList.add("card", "i.category", "hide");
+  card.classList.add("card", i.category, "hide");
 
   //  Create Image div
   let imgContainer = document.createElement("div");
@@ -95,3 +95,59 @@ for (let i of products.data) {
   // place container inside card
   card.appendChild(container);
 }
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+// initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
